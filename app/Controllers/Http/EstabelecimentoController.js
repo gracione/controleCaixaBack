ec494edeac93c85cdbfb08a5987f7838951665b9
gracione <gracione2017@gmail.com>
2023-08-14 21:00:47 -0300

    alterar

--- app/Controllers/Http/EstabelecimentoController.js
+++ app/Controllers/Http/EstabelecimentoController.js
@@ -25,6 +25,38 @@ class EstabelecimentoController {
 
     return response.status(201).json({ message: 'Estabelecimento criado com sucesso', estabelecimento: novoEstabelecimento })
   }
+
+  async getById ({ params, response }) {
+    const estabelecimento = await Estabelecimento.find(params.id)
+
+    if (!estabelecimento) {
+      return response.status(404).json({ message: 'Estabelecimento não encontrado' })
+    }
+
+    return response.json({ estabelecimento })
+  }
+
+  async atualizar ({ params, request, response }) {
+    const estabelecimento = await Estabelecimento.find(params.id)
+
+    if (!estabelecimento) {
+      return response.status(404).json({ message: 'Estabelecimento não encontrado' })
+    }
+
+    const data = request.only([
+      'nome',
+      'cnpj',
+      'endereco',
+      'contato',
+      'email',
+      'pin'
+    ])
+
+    estabelecimento.merge(data)
+    await estabelecimento.save()
+
+    return response.json({ message: 'Estabelecimento atualizado com sucesso', estabelecimento })
+  }
 }
 
 module.exports = EstabelecimentoController
